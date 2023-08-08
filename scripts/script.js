3da652cb590ed418d9262a985ff36c14d721a0f1
MrMadnessj <shivarama.off2@gmail.com>
2023-08-08 19:07:32 +0530

    Added form logic in js and stores data in console

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,35 @@
-const form = document.getElementById('book-form');
+// const form = document.getElementById('book-form');
 
-const submitBtn = document.getElementById('submit');
+// const submitBtn = document.getElementById('submit');
 
-let myLibrary = [];
-
-
-function Book(name, author, tpages, rpages, status){
-    this.name = name;
-    this.author = author;
-    this.tpages = tpages;
-    this.rpages = rpages;
-    this.status = status;
-}
+// let myLibrary = [];
 
 
-function addBookToLibrary(){
-    const name = document.getElementById('bname').value;
-    const author = document.getElementById('author').value;
-    const tpages = parseInt(document.getElementById('tpages').value);
-    const rpages = parseInt(document.getElementById('rpages').value);
-    const status = document.getElementById('status').value;
-
-    const newBook = new Book(name, author, tpages, rpages, status);
-
-    myLibrary.push(newBook);
-
-    console.log('Book Added :)');
-}
+// function Book(name, author, tpages, rpages, status){
+//     this.name = name;
+//     this.author = author;
+//     this.tpages = tpages;
+//     this.rpages = rpages;
+//     this.status = status;
+// }
 
 
+// function addBookToLibrary(){
+//     const name = document.getElementById('bname').value;
+//     const author = document.getElementById('author').value;
+//     const tpages = parseInt(document.getElementById('tpages').value);
+//     const rpages = parseInt(document.getElementById('rpages').value);
+//     const status = document.getElementById('status').value;
+
+//     const newBook = new Book(name, author, tpages, rpages, status);
+
+//     myLibrary.push(newBook);
+
+//     console.log('Book Added :)');
+// }
 
 
-
+//BOOK ADDING FORM
 const openformBtn = document.querySelectorAll('[data-form-target]');
 const closeformBtn = document.querySelectorAll('[data-close-button]');
 
@@ -63,4 +61,86 @@ function closeForm(form){
     if(form == null) return;
     form.classList.remove('active');
     overlay.classList.remove('active');
+    clearForm(addForm);
 }
+
+function clearForm(form){
+    bname.value = '';
+    author.value = '';
+    tpages.value = '';
+    cpages.value = '';
+}
+
+
+//LIBRARYDATA
+const addForm = document.getElementById("addForm");
+const addFormSubmit = document.getElementById('addform-sub');
+const error = document.querySelector('.error');
+
+
+const bookHolder = document.getElementsByClassName('books'); //bookgrid div
+
+let library = [];
+
+function Book(name, author, tpages, cpages){
+    this.name = name;
+    this.author = author;
+    this.tpages = tpages;
+    this.cpages = cpages;
+}
+
+
+function addBookToLibrary(){
+    const name = document.getElementById('bname').value;
+    const author = document.getElementById('author').value;
+    const tpages = document.getElementById('tpages').value;
+    const cpages = document.getElementById('cpages').value;
+
+    const newBook = new Book(name, author, tpages, cpages);
+
+    library.push(newBook);
+    console.log(library);
+    createBook(newBook);
+}
+
+function createBook(book){
+    
+}
+
+addForm.addEventListener('submit', (e) => {
+    let messages = [];
+    if(bname.value === '' || bname.value === null){
+        messages.push('Enter Name');
+    }
+
+    if(author.value === '' || author.value === null){
+        messages.push('Enter Author');
+    }
+
+    if(tpages.value === '' || tpages.value === null){
+        messages.push('Enter Total Pages')
+    }
+
+    if(cpages.value === '' || tpages.value === null){
+        messages.push('Enter Completed Pages')
+    }
+
+    if(parseInt(cpages.value) > parseInt(tpages.value)){
+        console.log(tpages.value);
+        console.log(cpages.value);
+
+        messages.push('Total Pages can\'t be smaller than Completed Pages');
+    }
+
+    if(messages.length > 0){
+        e.preventDefault();
+        error.innerText = messages.join(', ');
+    }
+
+    if(messages.length == 0)
+    {
+        e.preventDefault();
+        addBookToLibrary();
+        clearForm(addForm);
+    }
+})
